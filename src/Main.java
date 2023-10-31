495abab152addc4f9638ea60e0811240ee8be081
Alessio9999 <lexx8665@yandex.ru>
2023-10-31 08:50:03 +0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,53 @@
+public class Main {
+    public static void main(String[] args) {
+        Task1();
+        Task2();
+        Task3();
+        Task4();
+        Task5();
+        Task6();
+        Task7();
+        Task8();
+    }
+
+    private static void Task1() {
+        System.out.println("\nЗадача 1:");
+
+    }
+
+    private static void Task2() {
+        System.out.println("\nЗадача 2:");
+
+    }
+
+    private static void Task3() {
+        System.out.println("\nЗадача 3:");
+
+    }
+
+    private static void Task4() {
+        System.out.println("\nЗадача 4:");
+
+    }
+
+    private static void Task5() {
+        System.out.println("\nЗадача 5:");
+
+    }
+
+    private static void Task6() {
+        System.out.println("\nЗадача 6:");
+
+    }
+
+    private static void Task7() {
+        System.out.println("\nЗадача 7:");
+
+    }
+
+    private static void Task8() {
+        System.out.println("\nЗадача 8:");
+
+    }
+
+}
